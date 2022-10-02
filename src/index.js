6bc6da8168917c37df818532286cd26ed42bd179
Lucas Vallejos <lucascolegioripari@gmail.com>
2022-10-02 17:17:59 -0300

    I CHANGED EVERYTHING

--- src/index.js
+++ src/index.js
@@ -1,22 +1,78 @@
 const element = document.querySelector('.slider');
+const imgs = document.querySelectorAll('.frame')
+const dots = document.querySelectorAll('.dot')
 let rightPosition = "0px";
+let i = 0;
+
 
 const moveRight = function () { 
-   if (rightPosition !== "2100px"){
-      rightPosition = (parseInt(rightPosition == null ? 0 : rightPosition) + 700 )+"px"
-      element.style.right = rightPosition
+   if(element.classList.contains('active-left')){
+      element.classList.remove('active-left')
    }
-}
-const moveLeft = function () {
-   if(rightPosition !== "0px"){
+   element.classList.add('active-right')
+   if(imgs[i+1]){
       rightPosition = (parseInt(rightPosition == null ? 0 : rightPosition) - 700 )+"px"
       console.log(rightPosition)
-      element.style.right = rightPosition
+      element.style.transform = `translateX(${rightPosition})`
+      i++
    }
+   imgs.forEach(el => el.classList.remove('on'))
+   dots.forEach(el => el.classList.remove('clicked'))
+   imgs[i].classList.add('on')
+   dots[i].classList.add('clicked')
 }
+const moveLeft = function () {
+   if(element.classList.contains('active-right')){
+      element.classList.remove('active-right')
+   }
+   element.classList.add('active-left')
+   if(imgs[i-1]){
+      rightPosition = (parseInt(rightPosition == null ? 0 : rightPosition) + 700 )+"px"
+      console.log(rightPosition)
+      element.style.transform = `translateX(${rightPosition})`
+      i--
+   }
+   imgs.forEach(el => el.classList.remove('on'))
+   dots.forEach(el => el.classList.remove('clicked'))
+   imgs[i].classList.add('on')
+   dots[i].classList.add('clicked')
+}
+
+const dotNavigation = function () {
+   if(document.querySelector('.clicked')){
+      if(document.querySelector('.clicked').id < this.id){
+         for (let b = document.querySelector('.clicked').id; b < this.id; b++) {
+           moveRight()
+           
+         }
+      }
+      if(document.querySelector('.clicked').id > this.id){
+         for (let b = document.querySelector('.clicked').id; b > this.id; b--) {
+           moveLeft()
+           
+         }
+      }
+      
+
+   }
+   if(!document.querySelector('.clicked')){
+      for (let b = 0; b < this.id; b++) {
+           moveRight()
+           
+         }
+   
+   }
+
+   dots.forEach(el => el.classList.remove('clicked'))
+   this.classList.add('clicked')
+      
+}
+
 
 const nextButton = document.querySelector('.next')
 nextButton.addEventListener('click', moveRight)
 
 const previousButton = document.querySelector('.previous')
 previousButton.addEventListener('click', moveLeft)
+
+dots.forEach(el => el.addEventListener('click', dotNavigation))
